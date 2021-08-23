404dd71322f1650264e2f9279df04f6bb9435090
Jacob Adamson <jakeladamson@gmail.com>
2021-08-23 15:14:09 -0600

    commit initial setup

---
+++ index.js
@@ -0,0 +1,16 @@
+//import/include packages needed for app
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateHtml = require();
+const path = require("path");
+
+//create array of questions for user input
+
+const questions = [];
+
+//create function to write HTML file
+
+//create a function to iitialize app
+//present use with questions
+
+//function call to initialize app
